4ef97f4aeb32763876447c170b477c295b2832d1
Dmitrii Koshelev <me@koshelev.net>
2021-07-09 10:08:57 +0300

    Add Header, Main< Footer to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,14 @@
-import '../Layout';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import Layout from '../Layout/Layout';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <Layout>
-      
+      <Header />
+      <Main />
+      <Footer />
     </Layout>
   );
 }
