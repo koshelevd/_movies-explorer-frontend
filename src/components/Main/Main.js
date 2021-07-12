c189cfcb844d4070ec10b77679a84537552566b2
Dmitrii Koshelev <me@koshelev.net>
2021-07-12 16:02:13 +0300

    Develop Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,13 @@
+import Promo from '../Promo/Promo';
+
 import './Main.css';
 
 function Main() {
-  return <main className="main">  </main>;
+  return (
+    <main className="main">
+      <Promo />
+    </main>
+  );
 }
 
 export default Main;
