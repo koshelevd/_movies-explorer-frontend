c8819f9a23a4a6c26634c5be68358132c915eb63
Dmitrii Koshelev <me@koshelev.net>
2021-07-15 15:01:58 +0300

    Add / and /movies/ routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,21 @@
+import { Route } from 'react-router-dom';
+
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Layout from '../Layout/Layout';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
 
 function App() {
   return (
     <Layout>
       <Header />
-      <Main />
+      <Route exact path="/">
+        <Main />
+      </Route>
+      <Route path="/movies">
+        <Movies />
+      </Route>
       <Footer />
     </Layout>
   );
