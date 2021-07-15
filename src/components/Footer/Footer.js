fd965645378b4f14937685af47d67401d147f36c
Dmitrii Koshelev <me@koshelev.net>
2021-07-15 14:48:58 +0300

    Develop Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,35 @@
 import './Footer.css';
 
 function Footer() {
-  return <footer className="footer"> </footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__info">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
+      <div className="footer__bottom">
+        <p className="footer__copyright">&copy;2021</p>
+        <nav className="footer__nav">
+          <ul className="footer__links">
+            <li className="footer__links-item">
+              <a href="/" className="footer__link link smoothly">
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className="footer__links-item">
+              <a href="/" className="footer__link link smoothly">
+                Github
+              </a>
+            </li>
+            <li className="footer__links-item">
+              <a href="/" className="footer__link link smoothly">
+                Facebook
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
